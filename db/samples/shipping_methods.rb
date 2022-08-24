685d72716c6a3f577edf76832e6cd48bdb1a7175
Ucef <y.fkhar@yahoo.com>
2022-08-24 12:56:34 +0100

    update shipping methods

--- db/samples/shipping_methods.rb
+++ db/samples/shipping_methods.rb
@@ -1,61 +1,37 @@
 # frozen_string_literal: true
 
 begin
-north_america = Spree::Zone.find_by!(name: "North America")
+  morocco = Spree::Zone.find_or_create_by!(name: 'Morocco')
 rescue ActiveRecord::RecordNotFound
   puts "Couldn't find 'North America' zone. Did you run `rake db:seed` first?"
-  puts "That task will set up the countries, states and zones required for Spree."
+  puts 'That task will set up the countries, states and zones required for Spree.'
   exit
 end
 
-tax_category = Spree::TaxCategory.find_by!(name: "Default")
-europe_vat = Spree::Zone.find_by!(name: "EU_VAT")
+tax_category = Spree::TaxCategory.find_by!(name: 'Default')
+morocco_vat = Spree::Zone.find_by!(name: 'TVA')
 shipping_category = Spree::ShippingCategory.find_or_create_by!(name: 'Default')
 
 Spree::ShippingMethod.create!([
-  {
-    name: "UPS Ground (USD)",
-    zones: [north_america],
-    calculator: Spree::Calculator::Shipping::FlatRate.create!,
-    tax_category: tax_category,
-    shipping_categories: [shipping_category]
-  },
-  {
-    name: "UPS Two Day (USD)",
-    zones: [north_america],
-    calculator: Spree::Calculator::Shipping::FlatRate.create!,
-    tax_category: tax_category,
-    shipping_categories: [shipping_category]
-  },
-  {
-    name: "UPS One Day (USD)",
-    zones: [north_america],
-    calculator: Spree::Calculator::Shipping::FlatRate.create!,
-    tax_category: tax_category,
-    shipping_categories: [shipping_category]
-  },
-  {
-    name: "UPS Ground (EU)",
-    zones: [europe_vat],
-    calculator: Spree::Calculator::Shipping::FlatRate.create!,
-    tax_category: tax_category,
-    shipping_categories: [shipping_category]
-  },
-  {
-    name: "UPS Ground (EUR)",
-    zones: [europe_vat],
-    calculator: Spree::Calculator::Shipping::FlatRate.create!,
-    tax_category: tax_category,
-    shipping_categories: [shipping_category]
-  }
-])
+                                {
+                                  name: 'Amana',
+                                  zones: [morocco],
+                                  calculator: Spree::Calculator::Shipping::FlatRate.create!,
+                                  tax_category: tax_category,
+                                  shipping_categories: [shipping_category]
+                                },
+                                {
+                                  name: 'Aramex',
+                                  zones: [morocco],
+                                  calculator: Spree::Calculator::Shipping::FlatRate.create!,
+                                  tax_category: tax_category,
+                                  shipping_categories: [shipping_category]
+                                }
+                              ])
 
 {
-  "UPS Ground (USD)" => [5, "USD"],
-  "UPS Ground (EU)" => [5, "USD"],
-  "UPS One Day (USD)" => [15, "USD"],
-  "UPS Two Day (USD)" => [10, "USD"],
-  "UPS Ground (EUR)" => [8, "EUR"]
+  'Amana' => [40, 'MAD'],
+  'Aramex' => [50, 'MAD']
 }.each do |shipping_method_name, (price, currency)|
   shipping_method = Spree::ShippingMethod.find_by!(name: shipping_method_name)
   shipping_method.calculator.preferences = {
