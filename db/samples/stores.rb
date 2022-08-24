2150d545587eb35b9403e1e1566d20156a06fce2
Ucef <y.fkhar@yahoo.com>
2022-08-24 11:40:34 +0100

    change default store

--- db/samples/stores.rb
+++ db/samples/stores.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
-unless Spree::Store.where(code: 'sample-store').exists?
+unless Spree::Store.where(code: 'terroir-shop').exists?
   Spree::Store.create!(
-    name: "Sample Store",
-    code: "sample-store",
-    url: "example.com",
-    mail_from_address: "store@example.com"
+    name: "TerroirShop",
+    code: "terroir-shop",
+    url: "terroir-shop.local",
+    default: true,
+    mail_from_address: "shop@terroir-shop.local",
+    bcc_email: "contact@shop@terroir-shop.local",
+    default_currency: "MAD",
+    cart_tax_country_iso: "MA",
+    available_locales: ["fr", "en"],
+    meta_description: "La plateforme e-vitrine Terroir du Maroc, « TerroirShop » est une vitrine digitale 100% marocaine permettant l’accès à des produits du terroir marocain, en misant sur la qualité",
+    seo_title: "Terroir du Maroc"
   )
 end
